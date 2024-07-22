ecc3fcd8ebfeb6c415772b2066053be99f2a36b2
Whitemind109 <ganada38@naver.com>
2024-07-22 22:23:50 +0900

    Initial commit

---
+++ Source/Onslaught/Onslaught.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Onslaught : ModuleRules
+{
+	public Onslaught(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/Onslaught/Onslaught.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Onslaught.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Onslaught, "Onslaught" );
+ 
